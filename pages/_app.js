8bfae4d3e4d6c051f70b6ad061cec7fe1ed95537
Darren Thehamihardja <darren@192-168-1-17.tpgi.com.au>
2022-12-10 16:23:04 +1100

    google adsense test

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,7 @@ import Layout from '../components/layout/layout'
 import { QueryClient, QueryClientProvider } from 'react-query'
 import { DefaultSeo } from 'next-seo'
 import { defaultConfigSEO } from '../my.config'
+import Script from 'next/script'
 
 const queryClient = new QueryClient()
 
@@ -10,6 +11,15 @@ const MyApp = ({ Component, pageProps }) => {
   return (
     <QueryClientProvider client={queryClient}>
       <Layout>
+        <Script 
+          id="Adsense-id"
+          data-ad-client="ca-pub-6733088504690949"
+          async="true"
+          strategy="afterInteractive"
+          onError={ (e) => { console.error('Script failed to load', e) }}
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+          crossOrigin='anonymous'
+        />
         <DefaultSeo {...defaultConfigSEO} />
         <Component {...pageProps} />
       </Layout>
